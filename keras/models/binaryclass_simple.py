5828e60a948e7b50185403eaeba365ab00da4710
abhijay97 <abhijayvgupta@gmail.com>
2020-02-26 11:25:05 +0100

    model added

--- keras/models/binaryclass_simple.py
+++ keras/models/binaryclass_simple.py
@@ -1,6 +1,6 @@
-import keras
+from tensorflow import keras
 from layers.simple import GarNet
-
+from qkeras import *
 def make_model(n_vert, n_feat, n_class=2):
     n_aggregators = 4
     n_filters = 8
@@ -8,24 +8,25 @@ def make_model(n_vert, n_feat, n_class=2):
     
     x = keras.layers.Input(shape=(n_vert, n_feat))
     n = keras.layers.Input(shape=(1,), dtype='int32')
-    inputs = [x, n]
+    inputs = [x,n]
     
     v = inputs
-    v = [GarNet(4,8,8,input_format='xn', name='gar_1')(v), n]
-    v = [GarNet(4,8,8,input_format='xn', name='gar_2')(v), n]
-   # v = [GarNet(8,8,8,input_format='xn', name='gar_3')(v), n]
-   # v = [GarNet(8,8,8,input_format='xn', name='gar_4')(v), n]
-   # v = [GarNet(8,8,8,input_format='xn', name='gar_5')(v), n]
-   # v = [GarNet(8,8,8,input_format='xn', name='gar_6')(v), n]
-    v = GarNet(n_aggregators, n_filters, n_propagate, collapse='mean', input_format='xn', name='gar_7')(v)
-    v = keras.layers.Dense(8, activation='relu')(v)
+    #v = keras.layers.Flatten()(v)
+    
+    v = [GarNet(4,8,8,input_format='xn',name='gar_1')(v), n]
+    v = [GarNet(4,8,8, input_format='xn', name='gar_2')(v), n]
+    v = GarNet(n_aggregators, n_filters, n_propagate, collapse='mean', input_format='xn', name='gar_3')(v)
+    v = keras.layers.Dense(8, name='dense1')(v)
+    v = keras.layers.Activation('relu', name='act1')(v)
+    #v = QDense(8, kernel_quantizer=quantized_bits(1),bias_quantizer=quantized_bits(1), name="dense1")(v)
+    #v = QActivation("quantized_relu(32,16)", name="act1")(v)
     if n_class == 2:
         v = keras.layers.Dense(1, activation='sigmoid')(v)
     else:
         v = keras.layers.Dense(1, activation='softmax')(v)
     outputs = v
     
-    return keras.Model(inputs=inputs, outputs=outputs)
+    return keras.models.Model(inputs=inputs, outputs=outputs)
 
 def make_loss(n_class=2):
     if n_class == 2:
@@ -42,6 +43,6 @@ if __name__ == '__main__':
     n_feat = int(sys.argv[3])
 
     model = make_model(n_vert, n_feat, n_class=2)
-
+    print(model.summary())
     with open(out_path, 'w') as json_file:
         json_file.write(model.to_json())
--- keras/train_binaryclass.py
+++ keras/train_binaryclass.py
@@ -2,22 +2,19 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
-import tensorflow as tf
-#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+os.environ['CUDA_VISIBLE_DEVICES'] = '4'
+#import tensorflow as tf
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)
 #sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-#import keras
-#from keras import backend as K
+##import keras
+#from tensorflow.keras import backend as K
 #K.set_session(sess)
-from tensorflow import keras
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, TerminateOnNaN
-
-
 import sys
-#import keras
-
+from tensorflow import keras
+from tensorflow.keras.callbacks import Callback,EarlyStopping, ReduceLROnPlateau, TerminateOnNaN, ModelCheckpoint
+import numpy as np
 from models.binaryclass_simple import make_model, make_loss
-
+from layers.simple import GarNet
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
@@ -40,19 +37,31 @@ if __name__ == '__main__':
     #features = list(range(6))
     #features = [0, 1, 2, 3]
     features = None
-
+    #model2 = make_model(n_vert_max, n_feat=2, n_class=n_class)
+   # model_file = 'results3/model_dense.json'
+   # with open(model_file) as json_file:
+    #    json_config = json_file.read()
+    
+    #model2 = keras.models.model_from_json(json_config, custom_objects = {'GarNet':GarNet})
+    #model2.load_weights('results3/dense_weights.h5')
+    #print(' actually loaded')
+    #model2 = keras.models.load_model('results2/model_de.h5',custom_objects={'gar_1': GarNet, 'gar_2':GarNet})
+    print('loaded')
     model = make_model(n_vert_max, n_feat=4, n_class=n_class)
+    print(model.summary())
+    #model.layers[5].set_weights(model2.layers[5].get_weights())
+    model_json = model.to_json()
+    with open("results3/model_dense.json", "w") as json_file:
+        json_file.write(model_json)
+        
+    print('saving json')
     model_single = model
     if args.ngpu > 1:
         model = keras.utils.multi_gpu_model(model_single, args.ngpu)
     
-    optimizer = keras.optimizers.Adam(lr=0.005)
+    optimizer = keras.optimizers.Adam(lr=0.0005)
     
-    model.compile(optimizer=optimizer, loss=make_loss(n_class),metrics=['acc'])
-    print(model.summary())
-    model_json = model.to_json()
-    with open("model_binary_wtb.json", "w") as json_file:
-        json_file.write(model_json)
+    model.compile(optimizer=optimizer, loss=make_loss(n_class), metrics=['acc'])
 
     if args.use_generator:
         if args.input_type == 'h5':
@@ -61,17 +70,27 @@ if __name__ == '__main__':
             from generators.uproot_fixed import make_generator
         elif args.input_type == 'root-sparse':
             from generators.uproot_jagged_keep import make_generator
-
+        
+        #train_gen,n_train = make_generator(args.train_path, args.batch_size, format='xn', features=features, n_vert_max=n_vert_max, y_dtype=np.float, y_features=None, dataset_name=args.input_name)
         train_gen, n_train_steps = make_generator(args.train_path, args.batch_size, features=features, n_vert_max=n_vert_max, dataset_name=args.input_name)
-       # print(' this is train gen', train_gen)
-        fit_kwargs = {'steps_per_epoch': n_train_steps, 'epochs': args.num_epochs}
-
+        #fit_kwargs['steps_per_epoch'] = n_train_steps
+        print(train_gen)
+        #n_train_steps = 2734 
+        fit_kwargs = {'epochs': args.num_epochs, 'steps_per_epoch':n_train_steps}
+        #print("\n\n\n here \n\n\n",n_train_steps)
         if args.validation_path:
             valid_gen, n_valid_steps = make_generator(args.validation_path, args.batch_size, features=features, n_vert_max=n_vert_max, dataset_name=args.input_name)
             fit_kwargs['validation_data'] = valid_gen
             fit_kwargs['validation_steps'] = n_valid_steps
-
-        model.fit_generator(train_gen, **fit_kwargs, callbacks=[EarlyStopping(monitor='val_loss',patience=5),ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, verbose=1)])
+        md_s = ModelCheckpoint('/afs/cern.ch/work/a/abgupta/graph_f/g5/graph-hls-paper/keras/results3/dense_weights.h5',save_best_only=True, save_weights_only=True, monitor='val_loss', model='min', verbose=1)
+        callbacks = [EarlyStopping(monitor='val_loss',patience=7, verbose=1),ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1), md_s]
+        fit_kwargs['callbacks'] = callbacks
+        print(fit_kwargs)
+        model.fit_generator(train_gen, **fit_kwargs)
+        #print('qdense after train wt')
+        #print(model.layers[3].get_weights()[0])
+        model.reset_metrics()
+        #model.save_weights('model_test.h5', save_format='h5')
 
     else:
         if args.input_type == 'h5':
@@ -85,10 +104,14 @@ if __name__ == '__main__':
 
         fit_kwargs = {'epochs': args.num_epochs, 'batch_size': args.batch_size, 'shuffle': shuffle}
         if args.validation_path:
-            val_inputs, val_truth, _ = make_dataset(args.validation_path[0], format=input_format, features=features, n_vert_max=n_vert_max, y_features=y_features, dataset_name=args.input_name)
+            val_inputs, val_truth, _ = make_dataset(args.validation_path[0],format='xn', features=features, n_vert_max=n_vert_max, y_features=y_features, dataset_name=args.input_name)
             fit_kwargs['validation_data'] = (val_inputs, val_truth)
-
-        model.fit(inputs, truth, **fit_kwargs)
+        md_s = ModelCheckpoint('/afs/cern.ch/work/a/abgupta/graph_f/g5/graph-hls-paper/keras/results3/dense_weights.h5',save_best_only=True, save_weights_only=True, monitor='val_loss', model='min', verbose=1)
+        model.fit(inputs, truth, **fit_kwargs, callbacks=[md_s])
     
-    if args.out_path:
-        model_single.save(args.out_path)
+    #if args.out_path:
+        #model.reset_metrics()
+        #model.save(args.out_path)
+        #model.save_weights(args.out_path, save_format='h5')
+        #model.save_weights(filepath = args.out_path, save_format='h5')
+        #keras.models.save_model(model,args.out_path, save_format='h5')
